9aa7929810af5bbad49846f149ab48f0a3fad3a7
Pulkit Arora <pulkitaora0714@gmail.com>
2025-08-19 22:15:11 +0530

    email notification for revisit

--- SalesManagementSoftware/src/main/java/com/example/SalesManagementSoftware/services/impl/ReminderScheduler.java
+++ SalesManagementSoftware/src/main/java/com/example/SalesManagementSoftware/services/impl/ReminderScheduler.java
@@ -1,7 +1,7 @@
 package com.example.SalesManagementSoftware.services.impl;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,8 +20,8 @@ public class ReminderScheduler {
     @Autowired
     private EmailService emailService;
 
-    // Runs every day at 9 PM
-    @Scheduled(cron = "0 0 22 * * ?")
+    // Runs every day at 10
+    @Scheduled(cron = "0 0 10 * * ?")
     public void sendReminders() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
         List<VisitRecord> reports = reportRepository.findByDateOfRevisit(tomorrow);
@@ -32,7 +32,7 @@ public class ReminderScheduler {
                 String subject = "Reminder: Upcoming Visit Tomorrow";
                 String body = "Dear " + emp.getName() +
                               ",\n\nThis is a reminder that you have a scheduled revisit on " +
-                              report.getDateOfRevisit() + ".\n\nRegards,\nYour Company";
+                              report.getDateOfRevisit() + "for company " + report.getCompanyName() + ".\n\nRegards,\n Accounts Bureau";
 
                 emailService.sendEmail(emp.getEmail(), subject, body);
             }
