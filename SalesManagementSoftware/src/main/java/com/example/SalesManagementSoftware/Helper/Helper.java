ffae482f0a9fad9dcf7521b3f2b30d746641cbae
Pulkit Arora <pulkitaora0714@gmail.com>
2025-09-13 18:12:08 +0530

    final commit

--- SalesManagementSoftware/src/main/java/com/example/SalesManagementSoftware/Helper/Helper.java
+++ SalesManagementSoftware/src/main/java/com/example/SalesManagementSoftware/Helper/Helper.java
@@ -1,46 +1,48 @@
 package com.example.SalesManagementSoftware.Helper;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 public class Helper {
+
+    
     
     public static String getEmailOfLoggedInUser(Authentication authentication) {
-    Logger logger = LogManager.getLogger(Helper.class);
-    
-    if (authentication == null) {
-        return null; // or throw a custom exception
-    }
+        
+        Logger logger = LogManager.getLogger(Helper.class);
 
-    String email = null;
-
-    if (authentication instanceof OAuth2AuthenticationToken) {
-        var token = (OAuth2AuthenticationToken) authentication;
-        var registerId = token.getAuthorizedClientRegistrationId();
-        var principal = (DefaultOAuth2User) authentication.getPrincipal();
-
-        if ("google".equalsIgnoreCase(registerId)) {
-            email = principal.getAttribute("email");
-        } else {
-            logger.warn("OAuth2 provider not handled: " + registerId);
+        
+        if (authentication == null) {
+            return null; // or throw a custom exception
         }
 
-    } else {
-        email = authentication.getName();
-    }
+        String email = null;
 
-    return email;
-}
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            var token = (OAuth2AuthenticationToken) authentication;
+            var registerId = token.getAuthorizedClientRegistrationId();
+            var principal = (DefaultOAuth2User) authentication.getPrincipal();
+
+            if ("google".equalsIgnoreCase(registerId)) {
+                email = principal.getAttribute("email");
+            } else {
+                logger.warn("OAuth2 provider not handled: " + registerId);
+            }
+
+        } else {
+            email = authentication.getName();
+        }
+
+        return email;
+    }
 
 
     public static String getLinkForEmailVerification(String emailToken) {
 
-        String link = "http://abcrm.ap-south-1.elasticbeanstalk.com/auth/verify-email?token="  + emailToken;
+        String link = "http://localhost:8080/auth/verify-email?token=" + emailToken;
 
         return link;
     }
--- SalesManagementSoftware/src/test/java/com/example/SalesManagementSoftware/SalesManagementSoftwareApplicationTests.java
+++ SalesManagementSoftware/src/test/java/com/example/SalesManagementSoftware/SalesManagementSoftwareApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.SalesManagementSoftware;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SalesManagementSoftwareApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
